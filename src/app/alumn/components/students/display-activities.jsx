f3065d95de8481958529bd5561e1c0a6ec7c8a79
Zemogadrian <124641782+Zemogadrian@users.noreply.github.com>
2024-02-27 09:26:16 -0700

    Actualizar la visualización de actividades de los estudiantes

--- src/app/alumn/components/students/display-activities.jsx
+++ src/app/alumn/components/students/display-activities.jsx
@@ -7,24 +7,27 @@ export default function DisplayActivities () {
   const calificacion = '10'
 
   return (
-    <section className='h-14 w-3/4  bg-[#cdcbcc] '>
-      <div className='h-full flex flex-row items-center'>
-        <h1 className='w-full'>ACTIVIDAD</h1>
-        <button className='bg-[#808080] w-20 h-full items-center justify-center flex'>
-          <img src='/upload.svg' className='h-8' />
-        </button>
-        <p className='bg-[#2e9d36] w-20 h-full items-center justify-center flex'>
-          <img src='/accept.svg' className='h-8' />
-        </p>
-      </div>
-      <div className='flex flex-col text-[#cdcbcc] border-b-2 border-b-white py-10'>
-        <span>TEMA: {tema}</span>
-        <li>{actividad}</li>
-        <li>Fecha de entrega: {fechaEntrega}</li>
-        <li>{instrucciones}</li>
-        <li>Status: {status}</li>
-        <li>Calificación: {calificacion}</li>
-      </div>
-    </section>
+    <div className='flex flex-row'>
+      <section className='h-14 w-3/4  bg-[#cdcbcc] '>
+        <div className='h-full flex flex-row items-center'>
+          <h1 className='w-full'>ACTIVIDAD</h1>
+          <button className='bg-[#808080] w-20 h-full items-center justify-center flex'>
+            <img src='/upload.svg' className='h-8' />
+          </button>
+          <p className='bg-[#2e9d36] w-20 h-full items-center justify-center flex'>
+            <img src='/accept.svg' className='h-8' />
+          </p>
+        </div>
+        <div className='flex flex-col text-[#cdcbcc] border-b-2 border-b-white py-10 text-2xl'>
+          <span>TEMA: {tema}</span>
+          <li>{actividad}</li>
+          <li>Fecha de entrega: {fechaEntrega}</li>
+          <li>{instrucciones}</li>
+          <li>Status: {status}</li>
+          <li>Calificación: {calificacion}</li>
+        </div>
+      </section>
+
+    </div>
   )
 }
--- src/app/alumn/page.jsx
+++ src/app/alumn/page.jsx
@@ -2,11 +2,16 @@ import DisplayActivities from './components/students/display-activities'
 
 export default function Alumn () {
   return (
-    <main className='bg-gradient-to-tr from-[#1f5186] to-[#131a2d] w-full overflow-y-hidden'>
-      <section className='p-28'>
+    <main className='bg-gradient-to-tr from-[#1f5186] to-[#131a2d] w-full overflow-y-hidden flex flex-row'>
+      <section className='p-28 w-full'>
         <DisplayActivities />
       </section>
-
+      <section className='p-14'>
+        <select>
+          <option>Entregadas</option>
+          <option>No entregadas</option>
+        </select>
+      </section>
     </main>
   )
 }
