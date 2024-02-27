4c5153add85f954f8ae6376e4a3fa0639453adf3
KikeDeAlba <likingvirus1@gmail.com>
2024-02-26 20:04:12 -0700

    Fix button rendering issue in Days component

--- src/app/alumn/components/navbar/days.jsx
+++ src/app/alumn/components/navbar/days.jsx
@@ -15,7 +15,8 @@ export const Days = () => {
           key={v4()}
           onClick={() => setSelected(day)}
           className={`${selected === day ? 'text-[#808080] bg-[#fff] ' : 'text-[#fff] bg-[#808080] '}] text-xl font-bold rounded-full  w-10 h-10 bg-[#808080]  `}
-        >{day[0]}
+        >
+          {day[0]}
         </button>
       ))}
     </div>
