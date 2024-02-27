49d8a96dab9eeed7fc10236dd67052eb66b8ea1a
KikeDeAlba <likingvirus1@gmail.com>
2024-02-26 20:03:35 -0700

    Add unique key to buttons in Days component

--- src/app/alumn/components/navbar/days.jsx
+++ src/app/alumn/components/navbar/days.jsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { v4 } from '@/utils/uuid'
 import { useState } from 'react'
 
 const days = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes']
@@ -10,7 +11,11 @@ export const Days = () => {
   return (
     <div className='items-center justify-center flex gap-2'>
       {days.map((day, index) => (
-        <button key={index} onClick={() => setSelected(day)} className={`${selected === day ? 'text-[#808080] bg-[#fff] ' : 'text-[#fff] bg-[#808080] '}] text-xl font-bold rounded-full  w-10 h-10 bg-[#808080]  `}>{day[0]}
+        <button
+          key={v4()}
+          onClick={() => setSelected(day)}
+          className={`${selected === day ? 'text-[#808080] bg-[#fff] ' : 'text-[#fff] bg-[#808080] '}] text-xl font-bold rounded-full  w-10 h-10 bg-[#808080]  `}
+        >{day[0]}
         </button>
       ))}
     </div>
