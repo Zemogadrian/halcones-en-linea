fdb58d16bd005a83031c607c00d34703c4aa3bd1
KikeDeAlba <likingvirus1@gmail.com>
2024-02-27 20:48:32 -0700

    Add eslint rule and alt attribute to logo image

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -4,7 +4,7 @@ export default function Login () {
   return (
     <main className='w-screen h-screen flex flex-col p-10  gap-4 overflow-hidden' style={{ backgroundImage: 'url(/img/imagen2.jpg)', backgroundSize: 'cover' }}>
       <div className='w-full h-24 flex flex-row justify-between gap-2 items-center p-4 '>
-        <img src='/img/logoItesus.png' className=' h-24' />
+        <img src='/img/logoItesus.png' className=' h-24' alt='Itesus Logo' />
       </div>
       <section className='flex flex-col text-white justify-center text-2xl items-center py-48 '>
         <LoginForm />
