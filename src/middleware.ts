224c1a73da607b76ffe6e48dba7522307738b7db
KikeDeAlba <likingvirus1@gmail.com>
2024-02-28 22:08:13 -0700

    Refactor middleware function to handle user authentication and authorization

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,23 +8,20 @@ const LOGIN_PATHNAME = '/login'
 // This function can be marked `async` if using `await` inside
 export async function middleware (request: NextRequest) {
   const response = NextResponse.next()
-
   const pathname = new URL(request.url).pathname
-
   const supabase = await createClient(request, response)
-
   const { data, error } = await supabase.auth.getSession()
+
+  const isLogged = data?.session != null
 
   if (error != null) {
     return NextResponse.redirect(new URL(LOGIN_PATHNAME, request.url))
   }
 
-  const isLogged = data?.session != null
-
   const { data: userData } = await supabase.from('user_data').select('roles(*)').eq('owner', data.session?.user.id ?? 0).single()
-
   const redirectUrl = foundUserRedirect(userData?.roles?.id ?? 0)
 
+  // Si el usuario está logueado y trata de acceder a la página de login, redirigirlo a la página correspondiente
   if (pathname === LOGIN_PATHNAME && isLogged) {
     return NextResponse.redirect(new URL(redirectUrl, request.url))
   }
@@ -35,15 +32,10 @@ export async function middleware (request: NextRequest) {
   }
 
   // Si el usuario está logueado y trata de acceder a una pagina de la cual no tiene autorización, redirigirlo a la página correspondiente
-  if (isLogged) {
-    const isPermitEnter = userHasPermissionToEnter(userData?.roles?.id ?? 0, pathname)
+  const isPermitEnter = userHasPermissionToEnter(userData?.roles?.id ?? 0, pathname)
+  const permitFunction = (isPermitEnter && pathname !== '/') ? () => response : () => NextResponse.redirect(new URL(redirectUrl, request.url))
 
-    const permitFunction = isPermitEnter ? () => response : () => NextResponse.redirect(new URL(redirectUrl, request.url))
-
-    return permitFunction()
-  }
-
-  return response
+  return permitFunction()
 }
 
 // See "Matching Paths" below to learn more
