febe6f85e802eee0a3cd1a2c53fa60c7384f1a9c
KikeDeAlba <likingvirus1@gmail.com>
2024-03-04 18:33:27 -0700

    Add getCampuses function to retrieve campus data from Supabase

--- src/services/supabase/actions.ts
+++ src/services/supabase/actions.ts
@@ -68,3 +68,16 @@ export const getSubjects = async () => {
 export const getTopics = async () => {
   return []
 }
+
+export const getCampuses = async () => {
+  const supabase = await createClient()
+
+  const { data, error } = await supabase.from('campus').select('*')
+
+  if (error != null) {
+    console.error('Error getting campuses:', error)
+    throw new Error('Error getting campuses')
+  }
+
+  return data
+}
