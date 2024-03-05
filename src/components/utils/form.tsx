dadd6f4f5a3995e930d2579e0e727cea4279ac6b
KikeDeAlba <likingvirus1@gmail.com>
2024-03-04 18:46:31 -0700

    Update form component with optional action prop

--- src/components/utils/form.tsx
+++ src/components/utils/form.tsx
@@ -1,11 +1,12 @@
 
 interface Props {
   children: React.ReactNode
-  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void
+  onSubmit?: (e: React.FormEvent<HTMLFormElement>) => void
+  action?: (data: FormData) => Promise<void>
 }
 
-export const Form = ({ children, onSubmit }: Props) => (
-  <form onSubmit={onSubmit} className='w-full'>
+export const Form = ({ children, onSubmit, action }: Props) => (
+  <form onSubmit={onSubmit} action={action} className='w-full'>
     {children}
   </form>
 )
