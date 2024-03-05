cc87e7ef94c547c30d2306ec6fd648edc2b8af70
KikeDeAlba <likingvirus1@gmail.com>
2024-03-04 19:01:03 -0700

    Add form and input fields for creating a new career

--- src/app/admin/careers/new/page.tsx
+++ src/app/admin/careers/new/page.tsx
@@ -1,12 +1,42 @@
-import { FormSection, H1, Main } from '@/components/utils'
+import { Form, FormSection, H1, LabeledInput, Main } from '@/components/utils'
+import { getCampuses } from '@/services/supabase/actions'
+import { v4 } from '@/utils/uuid'
 
-export default function NewCareerPage () {
+export default async function NewCareerPage () {
+  const campus = await getCampuses()
+
+  console.log(campus)
+
   return (
     <Main>
       <H1>Crear carrera</H1>
 
       <FormSection>
-        a
+
+        <Form>
+          <LabeledInput
+            label='Nombre'
+            name='name'
+            type='text'
+            required
+            placeholder='Ingeniería en Sistemas Computacionales'
+          />
+
+          <LabeledInput
+            label='RVOE'
+            name='rvoe'
+            type='text'
+            required
+            placeholder='RVOE-123456'
+          />
+
+          <select>
+            {campus.map((campus) => (
+              <option key={v4()} value={campus.id}>{campus.name}</option>
+            ))}
+          </select>
+
+        </Form>
 
       </FormSection>
     </Main>
