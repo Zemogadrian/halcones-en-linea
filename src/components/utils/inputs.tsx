e0197cd85b470688d884574d72c6d6145ad306eb
KikeDeAlba <likingvirus1@gmail.com>
2024-03-05 11:20:35 -0700

    Refactor input and select components

--- src/components/utils/inputs.tsx
+++ src/components/utils/inputs.tsx
@@ -1,63 +1,70 @@
-export const LabeledInput = (
-  {
-    label,
-    name,
-    type = 'text',
-    placeholder,
-    required = false,
-    multiline = false
-  }:
-  {
-    label: string
-    name: string
-    type?: React.HTMLInputTypeAttribute
-    placeholder?: string
-    required?: boolean
-    multiline?: boolean
-  }
-) => !multiline
-  ? (
-    <label
-      className='flex flex-col gap-1 mt-1'
-    >
-      <span
-        className='text-white font-medium'
+type InputProps = React.InputHTMLAttributes<HTMLInputElement> & React.TextareaHTMLAttributes<HTMLTextAreaElement>
+
+interface LabeledInputProps extends InputProps {
+  label: string
+  name: string
+  type?: React.HTMLInputTypeAttribute
+  placeholder?: string
+  required?: boolean
+  multiline?: boolean
+}
+
+export const LabeledInput = ({
+  label,
+  name,
+  type = 'text',
+  placeholder,
+  required = false,
+  multiline = false,
+  ...props
+}: LabeledInputProps) =>
+  !multiline
+    ? (
+      <label
+        className='flex flex-col gap-1 mt-1'
       >
-        {label}
-      </span>
+        <span
+          className='text-white font-medium'
+        >
+          {label}
+        </span>
 
-      <input
-        type={type}
-        placeholder={placeholder}
-        name={name}
-        className='rounded-md px-2'
-        required={required}
-      />
-    </label>
-    )
-  : (
-    <label
-      className='flex flex-col gap-1 mt-1'
-    >
-      <span
-        className='text-white font-medium'
+        <input
+          type={type}
+          placeholder={placeholder}
+          name={name}
+          className='rounded-md px-2'
+          required={required}
+          {...props}
+        />
+      </label>
+      )
+    : (
+      <label
+        className='flex flex-col gap-1 mt-1'
       >
-        {label}
-      </span>
+        <span
+          className='text-white font-medium'
+        >
+          {label}
+        </span>
 
-      <textarea
-        placeholder={placeholder}
-        name={name}
-        className='rounded-md px-2'
-        required={required}
-      />
-    </label>
-    )
+        <textarea
+          placeholder={placeholder}
+          name={name}
+          className='rounded-md px-2'
+          required={required}
+          {...props}
+        />
+      </label>
+      )
 
-export const Select = ({ children, label }: {
+interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
   children: React.ReactNode
   label: string
-}) => (
+}
+
+export const Select = ({ children, label, ...props }: SelectProps) => (
   <label
     className='flex flex-col gap-1 mt-1'
   >
@@ -68,6 +75,7 @@ export const Select = ({ children, label }: {
     </span>
     <select
       className='w-full px-2 border py-1 border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-itesus-primary focus:border-transparent'
+      {...props}
     >
       {children}
     </select>
