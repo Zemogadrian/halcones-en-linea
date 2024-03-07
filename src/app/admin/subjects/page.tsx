2de52de4a8e9a73edf3e180afa70af09ba36a35e
KikeDeAlba <likingvirus1@gmail.com>
2024-03-07 14:16:10 -0700

    Add @tabler/icons-react package and implement new subject creation feature

--- src/app/admin/subjects/page.tsx
+++ src/app/admin/subjects/page.tsx
@@ -1,13 +1,29 @@
 import { H1, Main } from '@/components/utils'
 import { getSubjects } from '@/services/supabase/actions'
 import { v4 } from '@/utils/uuid'
+import { IconPlus } from '@tabler/icons-react'
+import Link from 'next/link'
 
 export default async function SubjectsPage () {
   const subjects = await getSubjects()
 
   return (
     <Main>
-      <H1>Materias</H1>
+      <div
+        className='flex items-center justify-between mb-4'
+      >
+        <H1
+          className='mb-0'
+        >Materias
+        </H1>
+
+        <Link
+          href='/admin/subjects/new'
+          className='p-1 bg-blue-500 rounded-md cursor-pointer hover:bg-blue-600 transition-colors duration-300 ease-in-out'
+        >
+          <IconPlus color='#fff' />
+        </Link>
+      </div>
 
       <ul
         className='flex-1 overflow-y-auto'
