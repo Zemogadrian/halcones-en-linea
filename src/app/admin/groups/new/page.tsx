78229155525ab1de6677798f05b4efbee51b5af0
KikeDeAlba <likingvirus1@gmail.com>
2024-03-13 15:15:16 -0700

    Add form for creating new groups

--- src/app/admin/groups/new/page.tsx
+++ src/app/admin/groups/new/page.tsx
@@ -1,9 +1,31 @@
-import { Main } from '@/components/utils'
+import { Form, FormSection, H1, LabeledInput, Main, Select, SubmitButton } from '@/components/utils'
+import { createGroup, getCareers } from '@/services/supabase/actions'
 
-export default function NewGroupPage () {
+export default async function NewGroupPage () {
+  const carears = await getCareers()
   return (
     <Main>
-      a
+      <H1 className='text-white'>Crear Grupo</H1>
+      <FormSection>
+        <Form action={createGroup}>
+          <LabeledInput
+            label='Grupo'
+            name='name'
+            type='text'
+            required
+            placeholder='Grupo A, Grupo B, etc...'
+          />
+          <Select
+            label='Carrera'
+            name='career'
+          >
+            {carears.map((career) => (
+              <option key={career.id} value={career.id}>{career.name}</option>
+            ))}
+          </Select>
+          <SubmitButton>Crear</SubmitButton>
+        </Form>
+      </FormSection>
     </Main>
   )
 }
