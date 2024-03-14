10c5bfea84de4565ced0c0e748c5cf918d201e66
KikeDeAlba <likingvirus1@gmail.com>
2024-03-14 11:05:06 -0700

    Add professor's careers section

--- src/app/admin/professor/view/[id]/page.tsx
+++ src/app/admin/professor/view/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { H1, H2, Main } from '@/components/utils'
 import { getProfessor, getProfessorCareers } from '@/services/supabase/actions'
 import { AddClass } from './components/add-class'
+import { v4 } from '@/utils/uuid'
 
 interface Props {
   params: {
@@ -17,13 +18,66 @@ export default async function ProfessorViewPage ({ params }: Props) {
 
   return (
     <Main>
-      <H1 className='capitalize text-white'>{professor.first_name}</H1>
 
-      <header className='flex justify-between'>
-        <H2 className='text-white'>Clases</H2>
-
+      <header className='flex justify-between mb-10'>
+        <div>
+          <H1 className='capitalize text-white'>{professor.first_name}</H1>
+        </div>
         <AddClass professorId={params.id} />
       </header>
+
+      <section>
+        <H2 className='text-white mb-4'>Carreras</H2>
+
+        <div className='grid grid-cols-3 gap-2'>
+          {careers.map(c => (
+            <article key={v4()}>
+              a
+            </article>
+          ))}
+        </div>
+
+        {/* <div className='grid grid-cols-3 gap-2'>
+          {careers.map((career) => (
+            <article key={v4()} className='bg-white p-4 rounded-lg'>
+              <H3 className='capitalize'>{career.name}</H3>
+
+              {career.educationPlans.map((plan) => (
+                <div key={v4()}>
+                  <H4 className='capitalize'>
+                    <span className='font-normal'>- </span>
+                    {plan.name}
+                  </H4>
+
+                  <div className='ml-4'>
+                    {plan.groups.map((group) => (
+                      <div key={v4()}>
+                        <H5><span>-</span> {group.name}</H5>
+
+                        <div className='ml-4'>
+                          {group.semesters.map((semesters) => (
+                            <div key={v4()}>
+                              <H6>Semestre {semesters.number}</H6>
+
+                              {semesters.subjects.map((subject) => (
+                                <div key={v4()}>
+                                  <p>{subject.name}</p>
+                                </div>
+                              ))}
+                            </div>
+                          ))}
+                        </div>
+
+                      </div>
+                    ))}
+                  </div>
+
+                </div>
+              ))}
+            </article>
+          ))}
+        </div> */}
+      </section>
     </Main>
   )
 }
--- src/components/utils/titles.tsx
+++ src/components/utils/titles.tsx
@@ -14,3 +14,35 @@ export const H2 = ({ children, className, ...props }: React.HTMLAttributes<HTMLH
     {children}
   </h2>
 )
+
+export const H3 = ({ children, className }: React.HTMLAttributes<HTMLHeadingElement>) => (
+  <h3
+    className={`text-xl font-medium ${className ?? ''}`}
+  >
+    {children}
+  </h3>
+)
+
+export const H4 = ({ children, className }: React.HTMLAttributes<HTMLHeadingElement>) => (
+  <h4
+    className={`text-lg font-medium ${className ?? ''}`}
+  >
+    {children}
+  </h4>
+)
+
+export const H5 = ({ children, className }: React.HTMLAttributes<HTMLHeadingElement>) => (
+  <h5
+    className={`text-base font-medium ${className ?? ''}`}
+  >
+    {children}
+  </h5>
+)
+
+export const H6 = ({ children, className }: React.HTMLAttributes<HTMLHeadingElement>) => (
+  <h6
+    className={`text-sm font-medium ${className ?? ''}`}
+  >
+    {children}
+  </h6>
+)
