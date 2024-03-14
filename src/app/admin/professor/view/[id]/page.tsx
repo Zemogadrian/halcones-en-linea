a0a65b6d9c8e0e9faf233b2f4ebebe629b415d40
KikeDeAlba <likingvirus1@gmail.com>
2024-03-14 14:54:28 -0700

    Remove console.log statement

--- src/app/admin/professor/view/[id]/page.tsx
+++ src/app/admin/professor/view/[id]/page.tsx
@@ -14,8 +14,6 @@ export default async function ProfessorViewPage ({ params }: Props) {
 
   const careers = await getProfessorCareers(params.id)
 
-  console.log(careers)
-
   return (
     <Main>
 
