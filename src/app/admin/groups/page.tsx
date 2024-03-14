e27a35d90529db4d8519732e9d19cb33b3485866
KikeDeAlba <likingvirus1@gmail.com>
2024-03-13 17:18:15 -0700

    Add table component for displaying groups

--- src/app/admin/groups/page.tsx
+++ src/app/admin/groups/page.tsx
@@ -1,14 +1,16 @@
 import { H1, Main } from '@/components/utils'
 import { getReducedCareers } from '@/services/supabase/actions'
 import { SelectCareers } from './components/select-career'
+import { TableGroups } from './components/table-groups'
+import { Suspense } from 'react'
 
 interface Props {
   searchParams: {
-    career: string
+    career?: string
   }
 }
 
-export default async function GroupsPage () {
+export default async function GroupsPage ({ searchParams }: Props) {
   const careers = await getReducedCareers()
 
   return (
@@ -17,6 +19,10 @@ export default async function GroupsPage () {
         <H1 className='text-white'>Grupos</H1>
         <SelectCareers careers={careers} />
       </header>
+
+      <Suspense fallback={<div>loading</div>}>
+        <TableGroups careerId={parseInt(searchParams.career ?? '0')} />
+      </Suspense>
     </Main>
   )
 }
