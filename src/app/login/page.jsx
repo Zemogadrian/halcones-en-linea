0b9c998a89080a0748b94a307c6ec581c5d4655f
KikeDeAlba <likingvirus1@gmail.com>
2024-03-23 15:29:36 -0700

    Add login page styles

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,8 +1,11 @@
 import { LoginForm } from './components/login-button'
+import styles from './styles.module.css'
 
 export default function Login () {
   return (
-    <main className='w-screen h-screen flex flex-col p-10  gap-4 overflow-hidden' style={{ backgroundImage: 'url(/img/imagen2.jpg)', backgroundSize: 'cover' }}>
+    <main
+      className={`w-screen h-screen flex flex-col p-10  gap-4 overflow-hidden ${styles['main-container']}`}
+    >
       <div className='w-full h-24 flex flex-row justify-between gap-2 items-center p-4 '>
         <img src='/img/logoItesus.png' className=' h-24' alt='Itesus Logo' />
       </div>
