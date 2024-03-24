72c21500c17a05429c1ed9f5bbdf601782f048f4
KikeDeAlba <likingvirus1@gmail.com>
2024-03-23 19:29:36 -0700

    Refactor navbar component

--- src/app/student/components/navbar/navbar.jsx
+++ src/app/student/components/navbar/navbar.jsx
@@ -9,15 +9,15 @@ export const NavBar = async ({ handleClick }) => {
   const hasMaterias = options.some(option => option.title === 'Materias')
 
   return (
-    <div className=' flex flex-row h-16 select-none bg-[#cdcbcc] justify-around items-center'>
+    <nav className=' flex flex-row h-16 select-none bg-[#cdcbcc] justify-around items-center'>
       <Days />
-      <div className='flex flex-row text-xl h-full justify-center '>
+      <section className='flex flex-row text-xl h-full justify-center '>
         {!hasMaterias && <CalAnimation />}
-      </div>
-      <div className='flex items-center gap-2 bg-[#b0b0b0] rounded-lg px-3 py-1'>
+      </section>
+      <section className='flex items-center gap-2 bg-[#b0b0b0] rounded-lg px-3 py-1'>
         <button className='text-[#394075] font-semibold capitalize'>{user?.first_name}</button>
         <Logout />
-      </div>
-    </div>
+      </section>
+    </nav>
   )
 }
