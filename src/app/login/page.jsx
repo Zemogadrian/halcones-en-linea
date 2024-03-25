8c99562a47e96a3410a0baff16fefaabc24e177d
KikeDeAlba <likingvirus1@gmail.com>
2024-03-25 12:30:28 -0700

    Update Itesus logo filename

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -7,7 +7,7 @@ export default function Login () {
       className={`w-screen h-screen flex flex-col p-10  gap-4 overflow-hidden ${styles['main-container']}`}
     >
       <div className='w-full h-24 flex flex-row justify-between gap-2 items-center p-4 '>
-        <img src='/img/logoItesus.png' className=' h-24' alt='Itesus Logo' />
+        <img src='/img/logo-itesus.png' className=' h-24' alt='Itesus Logo' />
       </div>
       <section className='flex flex-col text-white justify-center text-2xl items-center py-48 '>
         <LoginForm />
