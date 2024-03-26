38ff8cc4503a526e38add527653f7c5a8b55f0d0
KikeDeAlba <likingvirus1@gmail.com>
2024-03-26 10:38:41 -0700

    Refactor navbar component to use dynamic routes

--- src/app/student/components/navbar/nav-options.tsx
+++ src/app/student/components/navbar/nav-options.tsx
@@ -1,23 +1,76 @@
 'use client'
 
-import { usePathname } from 'next/navigation'
-import { SubjectOptions } from './subject-options'
+import { useParams, usePathname } from 'next/navigation'
+import { z } from 'zod'
+import Link from 'next/link'
+import { setCookie } from '@/services/actions'
 
 export const NavOptios = () => {
   const pathname = usePathname()
+  const params = useParams()
 
   const options = [
     {
       startWith: '/student/subject',
-      component: SubjectOptions
+      getRoutes: ({ params }) => {
+        const subjectId = z.coerce.string().parse(params.id)
+
+        return [
+          {
+            name: 'Temas',
+            href: `/student/subject/${subjectId}/topics`,
+            ref: 'topics'
+          },
+          {
+            name: 'Documentación',
+            href: `/student/subject/${subjectId}/documents`,
+            ref: 'documents'
+          },
+          {
+            name: 'Actividades',
+            href: `/student/subject/${subjectId}/activities`,
+            ref: 'activities'
+          },
+          {
+            name: 'Examen',
+            href: `/student/subject/${subjectId}/exam`,
+            ref: 'exam'
+          },
+          {
+            name: 'Clases grabadas',
+            href: `/student/subject/${subjectId}/recordedclasses`,
+            ref: 'recordedclasses'
+          }
+        ]
+      }
     }
   ]
 
-  const Component = options.find(({ startWith }) => pathname.startsWith(startWith))?.component
+  const routes = options.find(({ startWith }) => pathname.startsWith(startWith))?.getRoutes({ params })
 
-  if (Component == null) return null
+  if (routes == null) return null
+
+  const handleNav = (ref) => () => {
+    setCookie('calNav', ref)
+      .catch(err => console.error(err))
+  }
 
   return (
-    <Component />
+    <div className='flex flex-row text-xl h-full justify-center items-center'>
+      {routes.map(({ name, href, ref }, i) => (
+        <div className={`flex  ${i === (routes.length - 1) ? '' : 'border-r-2'} border-r-[#27316e] px-2 py-1`} key={name}>
+          <Link onClick={handleNav(ref)} href={href}>
+            <span
+              className={` px-2
+          ${pathname === href ? 'text-[#fff] bg-gradient-to-tr from-[#1f5186] to-[#131a2d] rounded-lg' : 'text-[#27316e]'}
+          hover:text-[#fff] hover:bg-gradient-to-tr from-[#1f5186] to-[#131a2d] hover:rounded-lg
+        `}
+            >
+              {name}
+            </span>
+          </Link>
+        </div>
+      ))}
+    </div>
   )
 }
