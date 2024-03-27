f66247e3a49adb42619f67e70a92fc7611cdcd53
Zemogadrian <124641782+Zemogadrian@users.noreply.github.com>
2024-03-27 11:32:49 -0700

    Actualizar tipos de base de datos y diseño de la barra de navegación del profesor

---
+++ src/app/professor/components/navbar/carrer.jsx
@@ -0,0 +1,16 @@
+'use client'
+import { useParams } from 'next/navigation'
+
+export const Carrer = async ({ handleClick }) => {
+  const params = useParams()
+  const decodedSlug = decodeURIComponent(params.slug)
+
+  const formattedSlug = decodedSlug
+    .split('-')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ')
+
+  return (
+    <h1 className='text-[#2c2b41] font-bold'>{formattedSlug}</h1>
+  )
+}
