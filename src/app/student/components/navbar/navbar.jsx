18004f24b910370c1008e4f746012d7445283949
KikeDeAlba <likingvirus1@gmail.com>
2024-03-27 10:35:47 -0700

    Refactor NavBar component

--- src/app/student/components/navbar/navbar.jsx
+++ src/app/student/components/navbar/navbar.jsx
@@ -2,9 +2,7 @@ import { ProfileSettingsButton } from '@/components/profile-settings-button'
 import { Days } from './days'
 import { NavOptios } from './nav-options'
 
-export const NavBar = async ({ handleClick }) => {
-  // const hasMaterias = newOptios.some(option => option.title === 'Materias')
-
+export const NavBar = async () => {
   return (
     <nav className=' flex flex-row h-16 select-none px-5 bg-[#cdcbcc] justify-between items-center'>
       <Days />
