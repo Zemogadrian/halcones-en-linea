5c2ccfd2399dcba46522a5fd40c7d4b08b33f404
KikeDeAlba <likingvirus1@gmail.com>
2024-03-27 10:35:30 -0700

    Update navbar styling to justify between items

--- src/app/student/components/navbar/navbar.jsx
+++ src/app/student/components/navbar/navbar.jsx
@@ -6,7 +6,7 @@ export const NavBar = async ({ handleClick }) => {
   // const hasMaterias = newOptios.some(option => option.title === 'Materias')
 
   return (
-    <nav className=' flex flex-row h-16 select-none px-5 bg-[#cdcbcc] justify-around items-center'>
+    <nav className=' flex flex-row h-16 select-none px-5 bg-[#cdcbcc] justify-between items-center'>
       <Days />
       <NavOptios />
       <ProfileSettingsButton />
