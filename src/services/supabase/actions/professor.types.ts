18e2d8975b54331393107ec380a1c0e00f898a59
KikeDeAlba <likingvirus1@gmail.com>
2024-03-28 11:45:16 -0700

    Refactor professor types and create activity props

--- src/services/supabase/actions/professor.types.ts
+++ src/services/supabase/actions/professor.types.ts
@@ -1,17 +1,24 @@
 import { Enums, TablesInsert } from 'database.types'
 
-interface Config<AT extends Enums<'activity_type'>> extends Omit<TablesInsert<'activities'>, 'id' | 'created_at' | 'type'> {
+interface Config<
+  AT extends Enums<'activity_type'>
+> extends Omit<TablesInsert<'activities'>, 'id' | 'created_at' | 'type'> {
   type: AT
 }
 
 type Response = Omit<TablesInsert<'multiple_options_responses'>, 'id' | 'created_at'>
 
-interface Question<QT extends Enums<'question_type'>> extends Omit<TablesInsert<'questions'>, 'id' | 'created_at' | 'type' | 'activity'> {
+interface Question<
+  QT extends Enums<'question_type'>
+> extends Omit<TablesInsert<'questions'>, 'id' | 'created_at' | 'type' | 'activity'> {
   type: QT
   responses: QT extends 'multiple_option' ? Response[] : null
 }
 
-export interface CreateActivityProps<AT extends Enums<'activity_type'>, QT extends Enums<'question_type'>> {
+export interface CreateActivityProps<
+  AT extends Enums<'activity_type'>,
+  QT extends Enums<'question_type'>
+> {
   config: Config<AT>
   questions: AT extends 'work' ? null : Array<Question<QT>>
 }
