25cae02130f0cc0cc43c03855e41153dfeb95bde
Zemogadrian <124641782+Zemogadrian@users.noreply.github.com>
2024-03-28 12:14:04 -0700

    Añadir archivos y realizar cambios en las páginas de actividades

---
+++ src/app/professor/career/[slug]/[subjectslug]/activities/create/components/options.jsx
@@ -0,0 +1,26 @@
+export const options = [
+  {
+    title: 'Trivia',
+    subtitle: 'Arma tu trivia',
+    description: 'Sigue las instrucciones en cada apartado para completar y asignar la trivia a tus alumnos correctamente',
+    component: 'trivia'
+  },
+  {
+    title: 'Examen',
+    subtitle: 'Arma tu examen',
+    description: 'Arma tu examen',
+    component: 'text'
+  },
+  {
+    title: 'Cuestionario',
+    subtitle: 'Arma tu cuestionario',
+    description: 'Arma tu cuestionario',
+    component: 'questionnaire'
+  },
+  {
+    title: 'Trabajo',
+    subtitle: 'Asigna trabajo o actividad',
+    description: 'Sigue las instrucciones en cada apartado para completar y asignar la actividad a tus alumnos correctamente',
+    component: 'work'
+  }
+]
---
+++ src/app/professor/career/[slug]/[subjectslug]/activities/create/work/components/deadline.jsx
@@ -0,0 +1,35 @@
+'use client'
+
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+
+export const DeadlineAct = async () => {
+  const { replace } = useRouter()
+  const params = useSearchParams()
+  const pathname = usePathname()
+
+  const handleOnChange = (e) => {
+    const queryParams = new URLSearchParams(params)
+
+    queryParams.set('activitydeadline', e.target.value)
+
+    const URL = `${pathname}?${queryParams.toString()}`
+    replace(URL)
+  }
+  return (
+    <div className='flex flex-col gap-10 justify-center items-center'>
+      <div className='flex flex-row gap-5'>
+        <button>
+          <img src='/arrow.svg' alt='' className='w-14 h-14 rotate-90' />
+        </button>
+        <input type='text' onChange={handleOnChange} placeholder='Fecha de entrega' className='underline-offset-auto text-white bg-transparent border-2 w-[30rem] h-40 text-center font-bold border-slate-500' />
+        <button>
+          <img src='/arrow.svg' alt='' className='w-14 h-14 -rotate-90' />
+        </button>
+      </div>
+      <button className='bg-[#1a62a6] text-white px-4 py-1 rounded-md'>
+        Asignar actividad
+      </button>
+    </div>
+
+  )
+}
