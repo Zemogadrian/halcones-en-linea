69c1c4146609969df85dcadb0bcb8df75833f10d
KikeDeAlba <likingvirus1@gmail.com>
2024-04-02 11:06:53 -0700

    Remove async keyword from NavBar component

--- src/app/student/components/navbar/navbar.tsx
+++ src/app/student/components/navbar/navbar.tsx
@@ -9,7 +9,7 @@ interface Props {
   user?: UserWithRoles | null
 }
 
-export const NavBar = async ({ options, user }: Props) => {
+export const NavBar = ({ options, user }: Props) => {
   return (
     <nav className=' flex flex-row h-16 select-none px-5 bg-[#cdcbcc] justify-between items-center'>
       <Days />
