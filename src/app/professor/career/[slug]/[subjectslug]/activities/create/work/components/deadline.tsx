43655c2db64901ee1a58a412b4756224584b7af8
Zemogadrian <124641782+Zemogadrian@users.noreply.github.com>
2024-04-02 16:35:57 -0700

    Eliminar importación innecesaria de Link en deadline.tsx

--- src/app/professor/career/[slug]/[subjectslug]/activities/create/work/components/deadline.tsx
+++ src/app/professor/career/[slug]/[subjectslug]/activities/create/work/components/deadline.tsx
@@ -1,6 +1,5 @@
 'use client'
 import { createActivity } from '@/services/supabase/actions/professors'
-import Link from 'next/link'
 import { useParams, usePathname, useRouter, useSearchParams } from 'next/navigation'
 import { useRef } from 'react'
 
