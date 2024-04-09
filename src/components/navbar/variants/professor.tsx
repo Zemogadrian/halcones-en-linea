82ec0fa19635cdd535811ac2ea6ce1d7bbe7f96b
KikeDeAlba <likingvirus1@gmail.com>
2024-04-09 10:46:34 -0700

    Update navbar and profile settings components to fix URL and redirect issues in student and professor career pages

--- src/components/navbar/variants/professor.tsx
+++ src/components/navbar/variants/professor.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { z } from 'zod'
 import { NavBarItem } from '../types'
 import { NavBar } from '../navbar'
 import { UserWithRoles } from '@/services/supabase/types'
@@ -9,48 +8,44 @@ import { startClass } from '@/services/supabase/actions/professors'
 
 const options: NavBarItem[] = [
   {
-    startWith: '/professor',
-    getRoutes: ({ params, queryParams }) => {
-      if (params.slug == null || params.subjectslug == null) return []
-
-      const slug = z.coerce.string().parse(params.slug)
-      const subjectslug = z.coerce.string().parse(params.subjectslug)
+    startWith: '/professor/career/[slug]/[subjectslug]',
+    getRoutes: ({ queryParams }) => {
       const queryParam = queryParamsSections.professorSubject
 
       return [
         {
           name: 'Horario',
-          href: `/professor/career/${slug}/${subjectslug}/schedule`,
+          href: '/professor/career/[slug]/[subjectslug]/schedule',
           ref: subjectRefs.schedule,
           queryParam
         },
         {
           name: 'Actividades',
-          href: `/professor/career/${slug}/${subjectslug}/activities`,
+          href: '/professor/career/[slug]/[subjectslug]/activities',
           ref: subjectRefs.activities,
           queryParam
         },
         {
           name: 'Foros',
-          href: `/professor/career/${slug}/${subjectslug}/forums`,
+          href: '/professor/career/[slug]/[subjectslug]/forums',
           ref: subjectRefs.forums,
           queryParam
         },
         {
           name: 'Temas y Documentación',
-          href: `/professor/career/${slug}/${subjectslug}/documentation`,
+          href: '/professor/career/[slug]/[subjectslug]/documentation',
           ref: subjectRefs.documentation,
           queryParam
         },
         {
           name: 'Calificaciones',
-          href: `/professor/career/${slug}/${subjectslug}/qualifications`,
+          href: '/professor/career/[slug]/[subjectslug]/qualifications',
           ref: subjectRefs.qualifications,
           queryParam
         },
         {
           name: 'Iniciar clase',
-          href: `/live-class/${slug}/${subjectslug}`,
+          href: '/live-class/[slug]/[subjectslug]',
           target: '_blank',
           onClick: () => {
             const convertToNumber = (value: string | null) => parseInt(value ?? '0')
