f4baf1814cd731199176b9c67bd92ef0fc0b9f30
Zemogadrian <124641782+Zemogadrian@users.noreply.github.com>
2024-04-19 16:15:23 -0700

    Actualiza el componente ShowImage para manejar múltiples archivos en la sección de actividades

--- src/app/professor/career/[slug]/[subjectslug]/activities/show-image.jsx
+++ src/app/professor/career/[slug]/[subjectslug]/activities/show-image.jsx
@@ -1,4 +1,5 @@
 'use client'
+import { v4 } from '@/utils/uuid'
 import { useState } from 'react'
 
 export default function ShowImage ({ selectedActivity }) {
@@ -49,9 +50,11 @@ export default function ShowImage ({ selectedActivity }) {
       <span className='text-[#8f8992]'>
         {selectedActivity?.name}
       </span>
-      <button className='' onClick={handleIncreaseWidth}>
-        <img src={selectedActivity?.files[0]?.url} alt='imgs' className='aspect-[16:9] w-96' />
-      </button>
+      {selectedActivity?.files?.map(file => (
+        <button className='' onClick={handleIncreaseWidth} key={v4()}>
+          <img src={file.url} alt='imgs' className='aspect-[16:9] w-96' />
+        </button>
+      ))}
       {isModalOpen && (
         <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50' onClick={handleCloseModal}>
           <div className='bg-white p-4'>
