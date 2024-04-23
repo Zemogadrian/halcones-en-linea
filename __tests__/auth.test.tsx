285f4a6d13ecf7361e8e8782f78d56ced9214205
KikeDeAlba <likingvirus1@gmail.com>
2024-04-23 11:33:02 -0600

    add vitest package

---
+++ __tests__/auth.test.tsx
@@ -0,0 +1,23 @@
+// import { expect, test } from 'vitest'
+// import { render, screen } from '@testing-library/react'
+// import Page from '../src/app/login/page'
+
+// test('succes login', () => {
+//   render(<Page />)
+
+//   const email = screen.getByPlaceholderText('Usuario')
+//   const password = screen.getByPlaceholderText('Contraseña')
+
+//   if (!(email instanceof HTMLInputElement) || !(password instanceof HTMLInputElement)) {
+//     throw new Error('Elements not found')
+//   }
+
+//   email.value = 'jedealbagaytan@gmail.com'
+//   password.value = '46983916'
+
+//   const submit = screen.getByRole('button', { name: 'Ingresar' })
+
+//   submit.click()
+
+//   expect(true).toBe(true)
+// })
